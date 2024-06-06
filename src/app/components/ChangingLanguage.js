8b99b1ce3c0e6d99cd4a288e3fa57094bc27942e
ITSPORTS <adpol95@gmail.com>
2024-06-06 16:38:38 +0300

    Decrease amount of code 1.1

--- src/app/components/ChangingLanguage.js
+++ src/app/components/ChangingLanguage.js
@@ -2,22 +2,31 @@ import {useRouter} from 'next/navigation';
 
 export default function ChangingLanguage(props) {
     const router = useRouter();
+    const data = [
+        {
+            lan: "ru",
+            text: "Русский"
+        },
+        {
+            lan: "en",
+            text: "English"
+        },
+        {
+            lan: "fr",
+            text: "French"
+        },
+    ]
 
     return (
-        <div className={props.lsa ? "hidden" : `flex flex-col absolute bg-orange text-white p-5 top-[64px] rounded-b-xl shadow-md ${props.anim ? "animate-slideIn" : "animate-slideOut"}`}>
+        <div
+            className={props.lsa ? "hidden" : `flex flex-col absolute bg-orange text-white p-5 top-[64px] rounded-b-xl shadow-md ${props.anim ? "animate-slideIn" : "animate-slideOut"}`}>
             <hr/>
-            <button type="button" onClick={() => {
-                router.push('/ru');
-                document.cookie = "lang=ru"+ ";path=/" + ";expires=Tue, 19 Jan 2038 03:14:07 GMT";
-            }}>Русский</button>
-            <button type="button" onClick={() => {
-                router.push('/en');
-                document.cookie = "lang=en"+ ";path=/" + ";expires=Tue, 19 Jan 2038 03:14:07 GMT";
-            }}>English</button>
-            <button type="button" onClick={() => {
-                router.push('/fr');
-                document.cookie = "lang=fr"+ ";path=/" + ";expires=Tue, 19 Jan 2038 03:14:07 GMT";
-            }}>French</button>
+            {
+                data.map(el => <button type="button" onClick={() => {
+                    router.push('/' + el.lan);
+                    document.cookie = "lang=" + el.lan + ";path=/" + ";expires=Tue, 19 Jan 2038 03:14:07 GMT";
+                }}> {el.text}</button>)
+            }
         </div>
     )
 }
