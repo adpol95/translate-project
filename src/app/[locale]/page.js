0310cd49f40744c680c72012fa0a4a4e8e9da5b1
ITSPORTS <adpol95@gmail.com>
2024-06-13 16:37:27 +0300

    Create dynamic routing system 1.0.1

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,8 +1,17 @@
-export default async function LanguagePic() {
+import Link from "next/link";
 
+export default async function Home({params}) {
+    console.log(params.locale)
     return (
-        <>
-        </>
+        <main className="pt-[30vh] pb-[18vh] flex-[1 1 auto]">
+            <section>
+                <Link href={`http://localhost:3000/${params.locale}/news`} className="text-[7vh] font-bold pb-[10vh] hover:text-orange transition-colors delay-30 active:text-orange-pick">
+                    <h1>
+                        {params.locale === "ru" ? "Новости" : "News"}
+                    </h1>
+                </Link>
+            </section>
+        </main>
     )
 }
 
