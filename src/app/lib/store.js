4a63f3996e1695c7d107ce2291017e4a42d4a639
ITSPORTS <adpol95@gmail.com>
2024-06-18 14:18:59 +0300

    Add counter with redux-toolkit

--- src/app/lib/store.js
+++ src/app/lib/store.js
@@ -1,9 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import themeReducer from "./features/themeSlice";
+import countReducer from "./features/countSlice";
 
 export const makeStore  = () => {
     return configureStore({
-        reducer: {theme: themeReducer},
+        reducer: {
+            theme: themeReducer,
+            count: countReducer
+        },
     })
 }
 
